00d0f7a33fe3563dfd0afe3768377668aa14f7b5
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-08-14 11:09:24 +0300

    first  python project

---
+++ lib/testing/app.py
@@ -0,0 +1,6 @@
+#print("hello word !",end="")
+#print("hello shercie !",end="!! ")
+#print("hello memer !",end="!!!!\n")
+#first_Number = 7
+#print(first_Number)
+print("Hello World! Pass this test, please")
