f5c86bd09b24d8bd92e331984caadd6ab8f74a9c
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-08-15 08:37:01 +0300

    pipenv setup

---
+++ lib/app.py
@@ -0,0 +1,8 @@
+# print("hello word !",end="")
+# print("hello shercie !",end="!! ")
+# print("hello memer !",end="!!!!\n")
+# first_Number = 7
+# print(first_Number)
+print("Hello World! Pass this test, please.")
+#dog_name ="zeus"
+#print(f"says hello to my {dog_name}")
--- lib/testing/app.py
+++
@@ -1,6 +0,0 @@
-#print("hello word !",end="")
-#print("hello shercie !",end="!! ")
-#print("hello memer !",end="!!!!\n")
-#first_Number = 7
-#print(first_Number)
-print("Hello World! Pass this test, please")
